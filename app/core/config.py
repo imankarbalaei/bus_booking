5942d97bd00463adcf82c6dd8a9e3ffe5a067b76
imankarbalaei <imankarbalaei82@gmail.com>
2025-10-28 22:58:00 +0330

    change Settings conf

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,10 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
     JWT_ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 1 day
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
     class Config:
         env_file = ".env"
